c165fcec88eb0227fbc5dd592a722fab7a852815
kynaruniverse <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>
2026-08-25 20:24:51 +0100

    update Color.kt

---
+++ app/src/main/java/com/remebrit/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.remebrit.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light theme (Part 5.3 of the master spec)
+val LightBackground = Color(0xFFFAF9F6)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFF3F1EC)
+val LightOnBackground = Color(0xFF252522)
+val LightOnSurfaceVariant = Color(0xFF6F6D67)
+val LightOutline = Color(0xFFE5E2DB)
+
+// Dark theme
+val DarkBackground = Color(0xFF171714)
+val DarkSurface = Color(0xFF20201C)
+val DarkSurfaceVariant = Color(0xFF292923)
+val DarkOnBackground = Color(0xFFF3F1E9)
+val DarkOnSurfaceVariant = Color(0xFFB7B4AA)
+val DarkOutline = Color(0xFF37362F)
+
+// Shared accent
+val AmberLight = Color(0xFFE7A83B)
+val AmberDark = Color(0xFFE9B04A)
